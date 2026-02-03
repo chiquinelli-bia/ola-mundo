865ed219955542f0cc7fcd71c7e3b3a5508ace0a
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-02-03 18:42:09 -0300

    feat(PaginaPadrao): cria componente PaginaPadrao e refatora projeto para implementação

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import "./App.css";
 import Menu from "@/componentes/Menu";
 import Inicio from "@/paginas/inicio/index.jsx";
 import Footer from "@/componentes/Footer";
+import PaginaPadrao from "@/componentes/PaginaPadrao";
 
 function App() {
   return (
     <BrowserRouter>
       <Menu></Menu>
       <Routes>
-        <Route path="/" element={<Inicio />}></Route>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />}></Route>
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
