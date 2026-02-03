9243639ed2d37da61a9e4d7181210a9a60bf0e2a
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-02-03 17:48:32 -0300

    fix(style): corrige estilos do componente Post

--- src/componentes/Post/index.jsx
+++ src/componentes/Post/index.jsx
@@ -2,9 +2,9 @@ import styles from "@/componentes/Post/post.module.css";
 
 export default function Post({ post }) {
   return (
-    <div className={styles.posts}>
+    <div className={styles.post}>
       <img
-        src={`/assets/posts/${post.id}/capa.png`}
+        src={`/public/posts/${post.id}/capa.png`}
         alt="imagem da capa do post"
         className={styles.capa}
       />
